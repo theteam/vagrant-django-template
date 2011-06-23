1a3768667a8553756685c8466a7d1454a8a0c4e5
Darian Moody <mail@djm.org.uk>
2011-06-23 14:18:48 +0100

    Base manifest converted to Ruby DSL; nginx templates updated with new variables.

--- manifests/base.rb
+++ manifests/base.rb
@@ -4,11 +4,34 @@ require 'yaml'
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
 
 node CONFIG['host_name'] do
-    include aptitude
-    include apache2
-    include logrotate
-    include memcached
-    include mysql
-    include nginx 
-    include python2
+
+    # Set all the variables we can calculate
+    # from the project configuration file.
+    settings = {
+        'client_name' => CONFIG['client_name'],
+        'project_name' => CONFIG['project_name'],
+        'python_project_name' => CONFIG['python_project_name'],
+        'domains' => CONFIG['domains'],
+        'static_url' => CONFIG['static_url'],
+        'media_url' => CONFIG['media_url'],
+    }
+
+    settings['full_project_name'] = "#{settings['client_name']}_#{settings['project_name']}"
+    settings['project_path'] = "/opt/#{settings['client_name']}/#{settings['project_name']}/"
+    settings['static_path'] = "#{settings['project_path']}current/#{settings['python_project_name']}/static/"
+    settings['media_path'] = "#{settings['project_path']}attachments/"
+
+    # Set them in the puppet scope for use
+    # lower down the manifest chain.
+    settings.each do |key, value|
+        scope.setvar(key, value)
+    end
+    
+    include 'aptitude'
+    include 'apache2'
+    include 'logrotate'
+    include 'memcached'
+    include 'mysql'
+    include 'nginx'
+    include 'python2'
 end
--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,17 +1,29 @@
 class nginx {
+
+    $sites_available_path = "/etc/nginx/sites-available/$full_project_name.conf" 
+    $sites_enabled_path = "/etc/nginx/sites-enabled/$full_project_name.conf"
+
     package { nginx: ensure => installed }
+
     service { nginx:
         ensure => running,
         enable => true
     }
+
     file {"/etc/nginx/nginx.conf":
         content => template("nginx/nginx.conf.erb"),
         require => Package["nginx"],
         notify => Service["nginx"]
     }
-    file {"/etc/nginx/sites-available/domains_production.conf":
+
+    file {$sites_available_path:
         content => template("nginx/nginx.conf.erb"),
         require => Package["nginx"],
-        notify => Service["nginx"]
+        notify => Service["nginx"],
+    }
+
+    file {$sites_enabled_path:
+        ensure => link,
+        target => $sites_available_path,
     }
 }
