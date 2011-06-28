61cae98bde27f90db092c74e068f5e39bc6cdf22
Darian Moody <mail@djm.org.uk>
2011-06-28 11:39:44 +0100

    Fixed createdb function. (escaping issue)

--- modules/mysql/manifests/createdb.pp
+++ modules/mysql/manifests/createdb.pp
@@ -1,7 +1,8 @@
 define mysql::createdb($db_name, $db_user, $db_pass) {
     exec { "create-$db_name-db":
-        unless => "/usr/bin/mysql -u$db_user -p$db_pass $db_name",
-        command => "/usr/bin/mysql -uroot -p$mysql_root_password -e 'CREATE DATABASE `$db_name`; CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_pass'; GRANT USAGE ON * . * TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON `$db_name` . * TO '$db_user'@'localhost'; FLUSH PRIVILEGES;",
+        # Only do this if the database doesn't already exist!
+        unless => "/usr/bin/mysql -uroot -p$mysql_root_password $db_name",
+        command => "/usr/bin/mysql -uroot -p$mysql_root_password -e \"CREATE DATABASE \\`$db_name\\`; CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_pass'; GRANT USAGE ON * . * TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON \\`$db_name\\` . * TO '$db_user'@'localhost'; FLUSH PRIVILEGES;\"",
         require => Exec["set-mysql-root-password"],
     }
 }
